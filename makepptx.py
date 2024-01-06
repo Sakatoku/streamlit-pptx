d4bf6495a86150d5e9b7072f554e5e3da21b9c2a
Sakatoku <akr.sakatoku@gmail.com>
2024-01-07 03:03:04 +0900

    Add other figures.

--- makepptx.py
+++ makepptx.py
@@ -2,10 +2,7 @@ from pptx import  Presentation
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
 from PIL import Image
-# import plotly
-# import kaleido
 
 # このプロトタイプで使用する定数
 OUTPUT_FILENAME = 'result.pptx'
@@ -14,20 +11,64 @@ TMP_DIR = 'tmp'
 SLIDE_WIDTH = 16
 SLIDE_HEIGHT = 9
 EMU_PER_INCH = 914400
- # EMU_PER_CM = 360000
 
-# PlotlyのFigureを作成する
-def generate_plotly_figure(df):
+# デモ用のグラフの生成関数(1種類目)：積み上げバーチャート
+def generate_example_figure1():
+    import plotly.express as px # type: ignore
     # ランダムな値が入ったデータを生成
-    x1 = np.random.randn(200) - 2
-    x2 = np.random.randn(200)
-    x3 = np.random.randn(200) + 2
-    hist_data = [x1, x2, x3]
-    group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-    # distplotでヒストグラムを描画する。Figure Factoryを使用してPlotlyのFigureを作成する
-    fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+    years = np.arange(1970, 2024, 1)
+    pref1 = ['Ehime', 7, 40]
+    pref2 = ['Tokushima', 6, 30]
+    pref3 = ['Kagawa', 5, 20]
+    pref4 = ['Kochi', 5, 20]
+    prefs = [pref1, pref2, pref3, pref4]
+    df = pd.DataFrame()
+    for pref in prefs:
+        tmp_df = pd.DataFrame({'prefecture': pref[0], 'year': years, 'value': np.random.randint(pref[1], pref[2], len(years))})
+        df = pd.concat([df, tmp_df])
+    # 積み上げバーチャートを生成する
+    fig = px.bar(df, x='year', y='value', color='prefecture',
+                color_discrete_sequence=px.colors.qualitative.T10,
+                barmode='stack', height=400)
     return fig
+
+# デモ用のグラフの生成関数(2種類目)：サンバーストチャート
+def generate_example_figure2():
+    import plotly.express as px # type: ignore
+    # データを読み込む
+    df = px.data.tips()
+    # サンバーストチャートを生成する
+    fig = px.sunburst(df, path=['sex', 'day', 'time'], values='total_bill', color='time',
+                                      color_discrete_sequence=px.colors.qualitative.T10
+        )
+    return fig
+
+# デモ用のグラフの生成関数(3種類目)：コロプレスマップ
+def generate_example_figure3():
+    import plotly.express as px # type: ignore
+    from urllib.request import urlopen
+    import json
+    import pandas as pd
+    # データを読み込む
+    with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+        counties = json.load(response)
+        df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv", dtype={"fips": str})
+        # コロプレスマップを生成する
+        fig = px.choropleth_mapbox(df,
+            geojson=counties,
+            locations='fips',
+            color='unemp',
+            color_continuous_scale="Viridis",
+            range_color=(0, 12),
+            mapbox_style="carto-positron",
+            zoom=3,
+            center={"lat": 37.0902, "lon": -95.7129},
+            opacity=0.5,
+            labels={'unemp':'unemployment rate'}
+        )
+        fig.update_layout(margin={"r":0, "t":0, "l":0, "b":0})
+    return fig
+    st.plotly_chart(fig, use_container_width=True)
 
 # 画像を中央に貼り付けるためのサイズを計算する
 def calculate_size(img, slide_size):
@@ -70,23 +111,28 @@ def make_presentation(imgs):
         slide = prs.slides.add_slide(layout)
         shapes = slide.shapes
         size = calculate_size(img, [prs.slide_width, prs.slide_height])
-        st.write(img)
-        st.write(size)
         _ = shapes.add_picture(img, size[0], size[1], size[2], size[3])
     
     return prs
 
-# Figureを作成する
-fig = generate_plotly_figure(None)
-st.plotly_chart(fig, use_container_width=True)
+# デモ用のグラフを生成する
+fig1 = generate_example_figure1()
+fig2 = generate_example_figure2()
+fig3 = generate_example_figure3()
+# 生成したグラフを表示する
+st.plotly_chart(fig1, use_container_width=True)
+st.plotly_chart(fig2, use_container_width=True)
+st.plotly_chart(fig3, use_container_width=True)
 
-# 作成したFigureを画像として保存する
-st.write('start to save image')
-fig.write_image(f'{TMP_DIR}/fig1.png', format='png', engine='orca')
-st.write('finish to save image')
+# 生成したグラフを画像として保存する
+img_pathlist = []
+for i, fig in enumerate([fig1, fig2, fig3]):
+    img_path = f'{TMP_DIR}/fig{i}.png'
+    fig.write_image(img_path, format='png', scale=2, engine='orca')
+    img_pathlist.append(img_path)
 
 # 画像を中央に貼り付けたプレゼンテーションを作成する
-prs = make_presentation([f'{TMP_DIR}/fig1.png'])
+prs = make_presentation(img_pathlist)
 
 # プレゼンテーションファイルを保存する
 prs.save(f'{OUTPUT_DIR}/{OUTPUT_FILENAME}')
